a02004d4c0d5f9b4696d221d233d8c3e047c3674
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-11 19:05:01 +0530

    Built in modules of nodejs

---
+++ 01_Modules/index.js
@@ -0,0 +1,17 @@
+// Modules
+
+// 1. Built in modules
+// 2. Third party modules
+// 3. Custom modules
+
+// 1. Built in modules
+const os = require('os');
+console.log(os.type());
+console.log(os.platform());
+console.log(os.homedir());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.cpus());
+
+const fs = require('fs');
+console.log(fs);
