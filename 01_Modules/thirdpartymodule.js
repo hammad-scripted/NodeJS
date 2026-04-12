5ed5a7ce54cbac2ec65a393302fc494c6f7cfc5d
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-12 10:45:51 +0530

    colors and moment module eg added

--- 01_Modules/thirdpartymodule.js
+++ 01_Modules/thirdpartymodule.js
@@ -1,1 +1,16 @@
+// ? Third party modules are modules that are not built in to Node.js, but can be installed using npm (Node Package Manager).
 
+// To install a third party module, you can use the following command in your terminal:
+// npm install <module-name>
+// For example, to install the 'colors' module, you can use the following command:
+// npm install colors
+
+// After installing the module, you can require it in your code and use its functionality. For example:
+const colors = require('colors');
+console.log('Hello World!'.green);
+console.log('Hello World!'.red);
+console.log('Hello World!'.blue);
+console.log('Hello World!'.yellow);
+
+const moment = require('moment');
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
