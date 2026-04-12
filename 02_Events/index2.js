43b15747ab325b64f160689e05d8730d1921af9c
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-12 11:43:01 +0530

    add example of using 'once' method with EventEmitter in User class

--- 02_Events/index2.js
+++ 02_Events/index2.js
@@ -16,3 +16,24 @@ chat.on('messageSent', (msg) => {
 
 // ? emit the 'messageSent' event and pass the message as an argument to the callback function
 chat.sendMessage('Hello World!');
+chat.sendMessage('How are you?');
+chat.sendMessage('Goodbye!');
+
+// Example of using 'once' method to listen to an event only once
+
+class User extends EventEmitter {
+  introduce(name) {
+    console.log(`My name is ${name}`.yellow);
+    this.emit('introduced', name);
+  }
+}
+
+// listen to the 'introduced' event only once
+const user = new User();
+user.once('introduced', (name) => {
+  console.log(`Welcome ${name} to our application!`.magenta);
+});
+
+// trigger the 'introduced' event
+user.introduce('Hammad');
+user.introduce('John'); // it will not be listened to because it was already listened to once
