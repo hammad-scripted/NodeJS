0a20a9fbaee5e167136796471954e6f9140e191b
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-12 10:39:06 +0530

    third party module eg added

--- 01_Modules/index.js
+++ 01_Modules/index.js
@@ -14,5 +14,18 @@ console.log(os.totalmem());
 console.log(os.cpus());
 
 const fs = require('fs');
-const content = fs.readFileSync('01_Modules/data.txt', 'utf-8');
+const content = fs.readFileSync('data.txt', 'utf-8');
 console.log(content);
+
+fs.writeFileSync('data.txt', 'Hello World!');
+const newContent = fs.readFileSync('data.txt', 'utf-8');
+console.log(newContent);
+
+const colors = require('colors');
+console.log('Hello World!'.green);
+console.log('Hello World!'.red);
+console.log('Hello World!'.blue);
+console.log('Hello World!'.yellow);
+
+// 2. Third party modules
+// 3. Custom modules
