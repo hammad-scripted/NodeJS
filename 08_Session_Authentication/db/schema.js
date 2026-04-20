6da2ad8da107dc78a3c2811d61beaaea341c955f
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-20 10:29:58 +0530

    deleted entire folder

--- 08_Session_Authentication/db/schema.js
+++
@@ -1,9 +0,0 @@
-import { integer, pgTable, varchar, text } from 'drizzle-orm/pg-core';
-
-export const usersTable = pgTable('users', {
-  id: uuid().primaryKey().defaultRandom(),
-  name: varchar({ length: 255 }).notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  password: text().notNull(),
-  salt:text().notNull(),
-});
