b8fa75046932063938522b6b78ddc087176dace5
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-21 16:06:24 +0530

    setup schema and model

--- 09_MongoDB/index.js
+++ 09_MongoDB/index.js
@@ -3,7 +3,9 @@ import { connectDb } from './connection.js';
 import 'dotenv/config';
 const app = express();
 
-connectDb(process.env.MONGODB_URL).then(() => console.log('Connected to DB'));
+connectDb(process.env.MONGODB_URL).then((conn) =>
+  console.log(`Connected to DB ${conn.connection.name}`),
+);
 app.listen(3000, () => {
   console.log(` Server is listening on PORT:3000`);
 });
---
+++ 09_MongoDB/models/user.model.js
@@ -0,0 +1,22 @@
+import { Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    age: {
+      type: Number,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = model('User', userSchema);
+export default userSchema;
